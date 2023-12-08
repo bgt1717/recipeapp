88e112092debf8e839d168bd4a675ebe8a72551a
bgt1717 <99308138+bgt1717@users.noreply.github.com>
2023-12-08 12:18:33 -0600

    start

---
+++ server/src/index.js
@@ -0,0 +1,14 @@
+// const express = require("express")
+import express from express; //modern notation, put' "type": "module",' in package.json. Express is a framework to create API. Serves frontend. 
+import cors from 'cors' // Rules between frontend and backend.
+import mongoose from 'mongoose' // Database management system, allows queries to database. 
+
+const app = express() //generate version of API.
+
+app.us(express.json()); //JSON middleware. data sent from frontend is converted into JSON with every request.
+app.use(cors()); //Resolves issues with API requests with frontend.
+
+app.listen(3001, () => console.log("server started")); //Tells API to start, has a call back function that logs server started in the console. 
+
+
+ 
