686e1af04273197cb0e26a4656f9d34e80dfb8e1
bgt1717 <99308138+bgt1717@users.noreply.github.com>
2023-12-08 15:30:07 -0600

    38:20

--- server/src/index.js
+++ server/src/index.js
@@ -3,7 +3,7 @@ import express from 'express'; //modern notation, put' "type": "module",' in pac
 import cors from 'cors'; // Rules between frontend and backend.
 import mongoose from 'mongoose'; // Database management system, allows queries to database. 
 
-import { userRouter } from '../routes/users.js';
+import { userRouter } from './routes/users.js';
 
 
 const app = express(); //generate version of API.
@@ -14,6 +14,8 @@ app.use(cors()); //Resolves issues with API requests with frontend.
 app.use("/auth", userRouter); // Seperating code to write endpoints related to authentication will exist in users.js
 
 mongoose.connect("mongodb+srv://bryce:oIsTcL1xm7UzIkLp@cluster0.jsoj83b.mongodb.net/Cluster0?retryWrites=true&w=majority") // Connects to server. 
+.then(() => console.log('Connected to MongoDB'))
+.catch(error => console.error('MongoDB connection error:', error));
 
 app.listen(3001, () => console.log("server started")); //Tells API to start, has a call back function that logs server started in the console. 
 
--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -1,12 +1,16 @@
 import express from "express";
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
+import { UserModel } from "../models/Users.js";
 
 const router = express.Router(); //set up router that exports an object that contains a router 
 
 //req gets all data from API request to endpoint.
 router.post("/register", async (req, res ) => {
     const {username, password} = req.body; // When making an API request in frontend, sends in an object containing username and password from the body.
+    const user = await UserModel.findOne({username:username}); //Finds the username in Database if it matches the username in the request body. If the two names are the same, can be shortend to one word. 
+
+    res.json(user); //Responds the user that was found.
 });
 router.post("/login");
 
