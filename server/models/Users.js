22db0a7bcec80d98625c1adc89091c66d7782fc8
bgt1717 <99308138+bgt1717@users.noreply.github.com>
2023-12-08 13:52:54 -0600

    30:00

---
+++ server/models/Users.js
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({ //Schema an object that defines structure of data.
+    username:{type: String, required: true, unique:true}, //username field that must be a string, it is required to be a string and must be unique. 
+    password: {type: String, required: true},
+});
+
+export const UserModel = mongoose.model("users", UserSchema) //Model generated based off schema. Setting Schema to be a collection. "users" is called from the database.
