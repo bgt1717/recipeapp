9f790d90ae43cc00e6d4a6b4fc11a195a31c2261
bgt1717 <99308138+bgt1717@users.noreply.github.com>
2023-12-12 11:05:41 -0600

    1:04

--- client/src/pages/auth.jsx
+++ client/src/pages/auth.jsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const Auth = () => {
   return (
-    <div>
-      auth
+    <div className="auth">
+      <Login />
+      <Register />
     </div>
-  )
-}
+  );
+};
 
-export default Auth
+const Login = () => {
+  return (
+    <div>
+      {/* Add Login form components here */}
+    </div>
+  );
+};
+
+const Register = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+
+  return (
+    <div className="auth-container">
+      <form> <h2>Register</h2>
+        <div className="form-group">
+          {/* html for refers to the input with the id called username */}
+          <label htmlFor='username'> Username: </label>
+          {/* The onchange event sets the username state to the value of the field anytime there's changes to the value in the field.  */}
+          <input type="text" id="username" value={username} onChange={(event) => setUsername(event.target.value)} />
+        </div>
+      </form>
+      <form> <h2>Register</h2>
+        <div className="form-group">
+          {/* html for refers to the input with the id called username */}
+          <label htmlFor='password'> Password: </label>
+          <input type="text" id="password" value={password} onChange={(event) => setPassword(event.target.value)} />
+        </div>
+      </form>
+
+    </div>
+  );
+};
+export default Auth;
