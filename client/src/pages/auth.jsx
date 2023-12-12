7c4d47fb1dc0ac293ad3b71a5e9719b20a9182e5
bgt1717 <99308138+bgt1717@users.noreply.github.com>
2023-12-12 13:01:10 -0600

    1:12:12

--- client/src/pages/auth.jsx
+++ client/src/pages/auth.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import axios from 'axios';
 
 const Auth = () => {
   return (
@@ -10,9 +11,8 @@ const Auth = () => {
 };
 
 const Login = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   return (
     <Form
@@ -25,10 +25,22 @@ const Login = () => {
   );
 };
 
-
 const Register = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axios.post("http://localhost:3001/auth/register", {
+        username, 
+        password,
+      }); //pass in object of username and password for the request.
+        alert("Registration Completed, please login.");
+    } catch (err) {
+      console.error(err); //makes error red.
+    }
+  };
 
   return (
     <Form
@@ -37,21 +49,20 @@ const Register = () => {
       password={password}
       setPassword={setPassword}
       label="Register"
+      onSubmit={onSubmit}
     />
   );
 };
 
-
-
-//The form has passed in props. 
-const Form = ({ username, setUsername, password, setPassword, label }) => {
+//The form has passed in props.
+const Form = ({ username, setUsername, password, setPassword, label, onSubmit }) => {
   return (
     <div className="auth-container">
-      <form>
+      <form onSubmit={onSubmit}>
         <h2>{label}</h2>
         <div className="form-group">
           {/* html for refers to the input with the id called username */}
-          <label htmlFor='username'> Username: </label>
+          <label htmlFor="username"> Username: </label>
           {/* The onchange event sets the username state to the value of the field anytime there's changes to the value in the field.  */}
           <input
             type="text"
@@ -62,8 +73,9 @@ const Form = ({ username, setUsername, password, setPassword, label }) => {
         </div>
         <div className="form-group">
           {/* html for refers to the input with the id called username */}
-          <label htmlFor='password'> Password: </label>
-          <input type="text"
+          <label htmlFor="password"> Password: </label>
+          <input
+            type="password"
             id="password"
             value={password}
             onChange={(event) => setPassword(event.target.value)}
@@ -73,9 +85,6 @@ const Form = ({ username, setUsername, password, setPassword, label }) => {
       </form>
     </div>
   );
-
-}
-
-
+};
 
 export default Auth;
