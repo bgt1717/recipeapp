a1c610dba7f302891ab189695bd6dc782259c638
bgt1717 <99308138+bgt1717@users.noreply.github.com>
2023-12-19 14:40:22 -0600

    1:47:50

--- client/src/pages/createRecipe.jsx
+++ client/src/pages/createRecipe.jsx
@@ -1,4 +1,18 @@
-import React from 'react'
+import {useState} from 'react';
+
+
+const[recipe, setRecipe] = useState({
+  name: "",
+  ingredients: [],
+  instructions: "",
+  cookingTime: 0,
+  userOwner: 0,
+});
+
+const handleChange = (event) => {
+  const{name, value} = event.target;
+  setRecipe({...recipe, [name]: value})    //function sets the handleChange for each of the onChange events. which in turn changes the setRecipe hook value for each recipe.  
+}
 
 const CreateRecipe = () => {
   return (
@@ -6,17 +20,17 @@ const CreateRecipe = () => {
       <h2>  create recipe</h2>
       <form>
         <label htmlFor="name"> Name </label>
-        <input type = "text" id="name" /> 
+        <input type = "text" id="name" name="name" onChange={handleChange} /> 
         <label htmlFor="description"> Description </label> 
-        <textarea id="description" name="description"></textarea>
+        <textarea id="description" name="description" onChange={handleChange}></textarea>
         <label htmlFor="ingredients">Ingredients</label>
         
         <label htmlFor="instructions"> Instructions</label>
-        <textarea id="instructions" name="instructions"> </textarea>
+        <textarea id="instructions" name="instructions" onChange={handleChange}> </textarea>
         <label htmlFor="imageUrl">Image URL</label>
-        <input type="text" id="imageUrl" name="imageUrl" />
+        <input type="text" id="imageUrl" name="imageUrl" onChange={handleChange}/>
         <label htmlFor="cookingTime">Cooking Time (Mins)</label>
-        <input type="number" id="cookingTime" name="cookingTime" />
+        <input type="number" id="cookingTime" name="cookingTime" onChange={handleChange}/>
       </form>
     
     </div>
