720ed4dcde67d81092f8a9f4bdc93d4585c0c0f6
bgt1717 <99308138+bgt1717@users.noreply.github.com>
2023-12-19 12:02:07 -0600

    1:44:05

--- client/src/pages/createRecipe.jsx
+++ client/src/pages/createRecipe.jsx
@@ -2,8 +2,23 @@ import React from 'react'
 
 const CreateRecipe = () => {
   return (
-    <div>
-      create recipe
+    <div className="create-recipe">
+      <h2>  create recipe</h2>
+      <form>
+        <label htmlFor="name"> Name </label>
+        <input type = "text" id="name" /> 
+        <label htmlFor="description"> Description </label> 
+        <textarea id="description" name="description"></textarea>
+        <label htmlFor="ingredients">Ingredients</label>
+        
+        <label htmlFor="instructions"> Instructions</label>
+        <textarea id="instructions" name="instructions"> </textarea>
+        <label htmlFor="imageUrl">Image URL</label>
+        <input type="text" id="imageUrl" name="imageUrl" />
+        <label htmlFor="cookingTime">Cooking Time (Mins)</label>
+        <input type="number" id="cookingTime" name="cookingTime" />
+      </form>
+    
     </div>
   )
 }
