3a2a150c1e321dfa03f4dd486ed58eb3a61bdb59
bgt1717 <99308138+bgt1717@users.noreply.github.com>
2023-12-19 15:05:42 -0600

    1:53:49

--- client/src/pages/createRecipe.jsx
+++ client/src/pages/createRecipe.jsx
@@ -1,40 +1,80 @@
-import {useState} from 'react';
-
-
-const[recipe, setRecipe] = useState({
-  name: "",
-  ingredients: [],
-  instructions: "",
-  cookingTime: 0,
-  userOwner: 0,
-});
-
-const handleChange = (event) => {
-  const{name, value} = event.target;
-  setRecipe({...recipe, [name]: value})    //function sets the handleChange for each of the onChange events. which in turn changes the setRecipe hook value for each recipe.  
-}
+import React, { useState } from "react";
 
 const CreateRecipe = () => {
+  const [recipe, setRecipe] = useState({
+    name: "",
+    ingredients: [],
+    instructions: "",
+    cookingTime: 0,
+    userOwner: 0,
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setRecipe({ ...recipe, [name]: value });
+  };
+
+  const handleIngredientChange = (event, idx) => {
+    const { value } = event.target;
+    const ingredients = recipe.ingredients;
+    ingredients[idx] = value;
+    setRecipe({ ...recipe, ingredients: ingredients });
+  };
+
+  const addIngredient = () => {
+    setRecipe({ ...recipe, ingredients: [...recipe.ingredients, ""] });
+  };
+
+  console.log(recipe);
+
   return (
     <div className="create-recipe">
-      <h2>  create recipe</h2>
+      <h2> create recipe</h2>
       <form>
         <label htmlFor="name"> Name </label>
-        <input type = "text" id="name" name="name" onChange={handleChange} /> 
-        <label htmlFor="description"> Description </label> 
-        <textarea id="description" name="description" onChange={handleChange}></textarea>
+        <input type="text" id="name" name="name" onChange={handleChange} />
+        <label htmlFor="description"> Description </label>
+        {recipe.ingredients.map((ingredient, idx) => (
+          <input
+            key={idx}
+            type="text"
+            name="ingredients"
+            value={ingredient}
+            onChange={(event) => handleIngredientChange(event, idx)}
+          />
+        ))}
+        <textarea
+          id="description"
+          name="description"
+          onChange={handleChange}
+        ></textarea>
         <label htmlFor="ingredients">Ingredients</label>
-        
+        <button type="button" onClick={addIngredient}>
+          Add Ingredient
+        </button>
         <label htmlFor="instructions"> Instructions</label>
-        <textarea id="instructions" name="instructions" onChange={handleChange}> </textarea>
+        <textarea
+          id="instructions"
+          name="instructions"
+          onChange={handleChange}
+        ></textarea>
         <label htmlFor="imageUrl">Image URL</label>
-        <input type="text" id="imageUrl" name="imageUrl" onChange={handleChange}/>
+        <input
+          type="text"
+          id="imageUrl"
+          name="imageUrl"
+          onChange={handleChange}
+        />
         <label htmlFor="cookingTime">Cooking Time (Mins)</label>
-        <input type="number" id="cookingTime" name="cookingTime" onChange={handleChange}/>
+        <input
+          type="number"
+          id="cookingTime"
+          name="cookingTime"
+          onChange={handleChange}
+        />
       </form>
-    
     </div>
-  )
-}
+  );
+};
 
-export default CreateRecipe
+export default CreateRecipe;
