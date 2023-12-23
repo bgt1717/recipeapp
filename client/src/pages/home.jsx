505858dd0187ac01bf8c17c03ccf3cd772121d8c
bryce <bgt17@txstate.edu>
2023-12-23 16:46:31 -0600

    2:11:36

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from 'react';
 import axios from "axios";
+import { useGetUserID } from '../hooks/useGetUserID';
 
 const Home = () => {
   const [recipes, setRecipes]=useState([]);
 
+  const userID = useGetUserID();
   useEffect(() => {
     const fetchRecipe = async () =>{
       try{
@@ -17,6 +19,18 @@ const Home = () => {
 
     fetchRecipe ();
   },[]);
+
+  const saveRecipe = async (recipeID) => {
+    try{
+      const response = await axios.put("http://localhost:3001/recipes",{
+        recipeID,
+        userID,
+      });
+      console.log(response);
+    } catch(err){
+      console.log(err);
+    }
+  }
   return (
    
     <div>
@@ -27,6 +41,7 @@ const Home = () => {
           <li key={recipe._id}>
             <div>
               <h2>{recipe.name}</h2>
+              <button onClick={() => saveRecipe(recipe._id)}> Save </button>
             </div>
             <div className="instructions">
               <p>{recipe.instructions}</p>
